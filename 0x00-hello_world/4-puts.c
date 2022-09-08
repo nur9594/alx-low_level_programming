f6681fa9170862af26e6fe136b4bd424ebdc6cb1
nur9594 <nhopen240112@gmail.com>
2022-09-08 14:49:46 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
    * /
   int main (void)
      {
-       puts("\"Programming is like building a multilingal puzzle \n");
-        return (0)
+       puts("\"Programming is like building a multilingal puzzle");
+        return (0);
       }
