973adc3111eff87a924e96ae77fb4a2cc4c78106
nur9594 <nhopen240112@gmail.com>
2022-09-08 14:44:37 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  /**
+   * main - printing
+   *
+   * Return : Always 0 (successful)
+   * /
+  int main (void)
+     {
+       puts("\"Programming is like building a multilingal puzzle \n");
+        return (0)
+      }
