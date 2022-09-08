77f4af0d87a17e30d926cfe0d9e0b7085f1cae06
nur9594 <nhopen240112@gmail.com>
2022-09-08 15:02:36 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+   #include <stdio.h>
+    
+
+          /**
+	   * main-prints atext
+	   *
+	   * Return: Always 0 (success)
+	   */
+                                  int main(void)
+	                       {
+				       printf("with proper grammer , but the outcome is a piece of art, \n");
+				       return 0;
+			       }
