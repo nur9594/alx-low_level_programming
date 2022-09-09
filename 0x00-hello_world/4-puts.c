45d5152e8555790163ec00dca40f8d75bcd69e50
nur9594 <nhopen240112@gmail.com>
2022-09-09 13:01:43 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <stdio.h> 
-/**
- * main - entry point;
- *
- * * return : 0 after finishing the function;
- * */
-	int main(void)
-		{
-			puts ("\"Programming is like building a multilingual puzzle");
-		       	return  (0);
-		
-		}
+#include <stdio.h>
+ /**
+  * Description : main - print out a string to stdout.
+  * Return : return 0 if success.
+  */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
