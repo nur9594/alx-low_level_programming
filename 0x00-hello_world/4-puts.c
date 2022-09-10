acd2d9f0a2da8ac05623bb1aa916c1df24ef0f89
nur9594 <nhopen240112@gmail.com>
2022-09-10 07:41:20 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * Main - print out a string to stdout.
- * Description - printing value of puts.
+ * Main - printing value of puts.
  * Return - return 0 if success.
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("%c\"programming is like building a multilingual puzzle");
 	return (0);
 }
