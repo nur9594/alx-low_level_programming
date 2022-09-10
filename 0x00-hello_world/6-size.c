989acb4a3530a48200223b3069f3ebf8abd01e08
nur9594 <nhopen240112@gmail.com>
2022-09-10 12:45:03 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Description: main - printout the size of variables type.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+{
+printf("size of char: %ld byte(s)\n", sizeof(a));
+printf("size of int: %ld byte(s)\n", sizeof(b));
+printf("size of long int: %ld byte(s)\n", sizeof(c));
+printf("size of long long int: %ld byte(s)\n", sizeof(d));
+printf("size of float: %ld byte(s)\n", sizeof(e));
+return (0);
+}
