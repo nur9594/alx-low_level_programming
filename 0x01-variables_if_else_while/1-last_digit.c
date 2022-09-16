6bf5875d9e6d55634e7ea8697853fdfe0fa165c4
nur9594 <nhopen240112@gmail.com>
2022-09-16 01:15:23 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * Main - print if the last digit is zero, greater than five and less than six
  * Description - using the main function
  * this program prints  the last digit is zero, greater than five and less than six 
- * Return :0
+ * Return - 0 if success
  */
 int main(void)
 {
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 l = n % 10;
 if(l > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n",n,l);
+printf("last digit of %d is %d and is greater than 5\n", n,l);
 }
 else if(l  == 0)
 {
-printf("last digit of %d is %d and is 0\n",n,l);
+printf("last digit of %d is %d and is 0\n", n,l);
 }
 else if(l < 6)
 {
-printf("last digit of %d is %d and and is less than 6 and not 0\n",n,l);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n,l);
 }
 return (0);
 }
